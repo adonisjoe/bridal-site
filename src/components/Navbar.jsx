d72f5989cbd80fb7d38c75cedf37a611155b1f77
Adonis Joe <emekajoe5@gmail.com>
2024-02-09 02:24:46 +0100

    set up useEffect to control sticky func on Navigation bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isSticky, setIsSticky] = useState(false);
+
+  //set toggle Menu
+  // const toggleMenu =
+  useEffect(() => {
+    const handleScroll = () => {
+      if (Window.scrollY > 100) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.addEventListener('scroll', handleScroll);
+    };
+  });
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
