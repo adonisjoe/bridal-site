93f44b6d9fdc7523aa5e88f411e833f10fbccae8
Adonis Joe <emekajoe5@gmail.com>
2024-02-09 21:35:08 +0100

    add service title and description

---
+++ src/components/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div className='md:px-14 px-4 py-16 max-w-screen-2xl mx-auto'>
+      <div className='text-center my-8'>
+        <h2>Our Bridal Services</h2>
+        <p className='text-neutralGrey'>Our variety of services to your need</p>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
