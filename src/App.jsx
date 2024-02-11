f64048499c3c524c17707896b06d32bbebccf5e0
Adonis Joe <emekajoe5@gmail.com>
2024-02-11 16:47:05 +0100

    fix warning text from netlify

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Services from './components/Services';
 import Faq from './components/Faq';
-import Footer from './components/Footer';
+import { Foot } from './components/Footer';
 
 function App() {
   return (
@@ -12,7 +12,7 @@ function App() {
       <Home />
       <Services />
       <Faq />
-      <Footer />
+      <Foot />
     </>
   );
 }
